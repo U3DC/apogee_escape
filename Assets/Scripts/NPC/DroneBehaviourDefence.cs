bfce12b317bdf44cb996032b2ff1a4c9d3a23eb4
Guy <guy.noble@gmail.com>
2018-08-10 21:53:09 +1000

    Improved defender drone behaviour

--- Assets/Scripts/NPC/DroneBehaviourDefence.cs
+++ Assets/Scripts/NPC/DroneBehaviourDefence.cs
@@ -4,118 +4,165 @@ using UnityEngine;
 
 public class DroneBehaviourDefence : MonoBehaviour
 {
-
-    public GameObject ship;
-    private Vector3 target;
-    private float angle;
-    private SphereCollider mySC;
-
-
-    [Header("Properties")]
-    public float rateOfFire = 2f;
+    [Header("Action")]
+    public GameObject weaponTrail;
+    public float weaponTrailTime = 0.2f;
+    public float rateOfFire = 1.2f;
     private bool canShoot = true;
     public int damageAmount = 40;
+    private SphereCollider mySC;
 
-    [Header("Randomisations - Movement")]
+    private enum droneState
+    {
+        patrolling,
+        attacking
+    };
+    [SerializeField]
+    private droneState currentState;
+    public float orbitChangeCountdown = 7f;
+
+    [Header("Movement")]
+    public float maxSpeed = 3f;
+    public float minSpeed = 1.5f;
+    private float speed;
+    public float radiusSpeed;
+
     public float maxRadius = 4f;
     public float minRadius = 8f;
     private float radius;
 
-    public float maxSpeed = 3f;
-    public float minSpeed = 1.5f;
-    private float speed;
+    public float angle;
 
-    [Header("Randomisations - Placement")]
+    [Header("Position")]
     public float maxXOffset = 10f;
     public float minXOffset = 0f;
-    private float xOffset;
-
     public float maxYOffset = 5f;
     public float minYOffset = -5f;
-    private float yOffset;
-
     public float maxZOffset = 5f;
     public float minZOffset = -5f;
-    private float zOffset;
+
+    // the vertical amount so that not all the drones are flying at the same altitude
+    public float maxYFlyOffset = 2f;
+    public float minYFlyOffset = -3f;
+    private float yFlyOffset;
 
     private Vector3 offsetPosition;
+    public GameObject ship;
+    public GameObject[] defencePositions;
+    private Vector3 nextDefencePosition;
+    public float maxDistance = 3f;
 
     void Start()
     {
-        SetRandomisations();
-        StartCoroutine("RateOfFireTimer");
-    }
-    IEnumerator RateOfFireTimer()
-    {
-            yield return new WaitForSeconds(rateOfFire);
-            canShoot = true;
-            yield break;
+        currentState = droneState.patrolling;
+        mySC = GetComponent<SphereCollider>();
     }
 
     void SetRandomisations()
     {
         radius = Random.Range(minRadius, maxRadius);
         speed = Random.Range(minSpeed, maxSpeed);
-        xOffset = Random.Range(minXOffset, maxXOffset);
-        yOffset = Random.Range(minYOffset, maxYOffset);
-        zOffset = Random.Range(minZOffset, maxZOffset);
 
         //Add to the ship position to create a new centre of orbit.
-        offsetPosition = new Vector3(xOffset, yOffset, zOffset) + ship.transform.position;
-        mySC = GetComponent<SphereCollider>();
+        offsetPosition = new Vector3(
+            Random.Range(minXOffset, maxXOffset),
+            Random.Range(minYOffset, maxYOffset),
+            Random.Range(minZOffset, maxZOffset)
+        );
+        nextDefencePosition = defencePositions[Random.Range(0, defencePositions.Length)].transform.position;
+        yFlyOffset = Random.Range(maxYFlyOffset, maxYFlyOffset);
     }
 
     void Update()
     {
-        OrbitMovement();
+        if (currentState == droneState.attacking)
+        {
+            transform.position = transform.position;
+        }
+        else if (currentState == droneState.patrolling)
+        {
+            OrbitMovement();
+        }
     }
 
     void OrbitMovement()
     {
-        // Get the direction from centre to our position
-        Vector3 dir = transform.position - offsetPosition;
+        if (currentState == droneState.patrolling)
+        {
+            if (Vector3.Distance(transform.position, ship.transform.position) > maxDistance)
+            {
+                Vector3 velocity = Vector3.zero;
+                //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(ship.transform.position.x, transform.position.y, ship.transform.position.z),ref velocity,0.3f);
+                transform.position = Vector3.Lerp(transform.position, new Vector3(ship.transform.position.x, transform.position.y, ship.transform.position.z), 0.1f * Time.deltaTime);
+                SetRandomisations();
 
-        // Calculate vector angel
-        angle = Mathf.Atan2(dir.z, dir.x);
-
-        // Translate to a point ahead of the orbit
-        angle += speed * Mathf.Deg2Rad;
-
-        // Calculate the vector3 position along the circle
-        target = new Vector3(
-            (Mathf.Cos(angle) * radius), 
-            dir.y,
-            (Mathf.Sin(angle) * radius)
-        ) + offsetPosition;
-
-        gameObject.transform.position = target;
+            }
+            else
+                transform.RotateAround(nextDefencePosition, Vector3.up, angle * Time.deltaTime);
+        }
     }
 
     void OnTriggerStay(Collider other)
     {
+        Debug.Log("alien detected");
         if (other.gameObject.tag == "Alien")
         {
+            StopCoroutine("ReturnToPatrolling");
+            currentState = droneState.attacking;
+
             if (canShoot == true)
             {
-                Debug.DrawLine(gameObject.transform.position, other.gameObject.transform.position, Color.red);
 
-                other.GetComponent<EnemyController>().damage(damageAmount);
+                GameObject trail;
+                trail = Instantiate(weaponTrail, transform.position, Quaternion.Euler(Vector3.zero), transform);
+                LineRenderer lr = trail.GetComponent<LineRenderer>();
+
+                if (lr != null)
+                {
+                    lr.SetPosition(0, transform.position);
+                    lr.SetPosition(1, other.transform.position);
+                }
+
+                if (currentState != droneState.attacking)
+                {
+                    Destroy(trail.gameObject);
+                }
+                else
+                {
+                    Destroy(trail.gameObject, weaponTrailTime);
+                }
+
+                if (other.GetComponent<EnemyController>() != null)
+                {
+                    other.GetComponent<EnemyController>().damage(damageAmount);
+                }
                 canShoot = false;
                 StartCoroutine("RateOfFireTimer");
+                StartCoroutine("ReturnToPatrolling");
+
             }
-
-
-
-
-        }   
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.DrawLine(gameObject.transform.position, other.gameObject.transform.position, Color.cyan);
-
-
-
-
         }
     }
 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Alien")
+        {
+            currentState = droneState.patrolling;
+        }
+    }
+
+    IEnumerator RateOfFireTimer()
+    {
+        yield return new WaitForSeconds(rateOfFire);
+        canShoot = true;
+        yield break;
+    }
+
+    IEnumerator ReturnToPatrolling()
+    {
+        yield return new WaitForSeconds(rateOfFire * 2f);
+        currentState = droneState.patrolling;
+        yield break;
+    }
 }
